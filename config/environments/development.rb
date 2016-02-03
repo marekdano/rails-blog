ca058d4f0b5fb0d39fb44855f4e7cdd859b4a12c
Marek Dano <mk.dano@gmail.com>
2016-02-03 22:53:32 +0000

    Add devise, cancancan and rolify gems. Add logic to the navbar when the user is or isn't logged in.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Defined default url options in development environment for DEVISE gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
